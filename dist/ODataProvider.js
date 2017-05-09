150aee3bba657a65fc366fe234b943b933fe480c
f <_._@conduce.net>
2017-05-09 12:29:28 +0100

    ODataProvider in TypeScript

---
+++ dist/ODataProvider.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var ODataProvider = (function () {
+    function ODataProvider() {
+    }
+    ODataProvider.prototype.Where = function (predicate) { return null; };
+    return ODataProvider;
+}());
+exports.ODataProvider = ODataProvider;
